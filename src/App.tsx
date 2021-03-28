4389f9f6ac1385a5d9510c23b8969a85d6b36daf
mitsuishidemi <hidemi.mitsuishi@gmail.com>
2021-03-28 08:19:00 +0200

    add emotion.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import React from 'react';
-import welcomeIcon from './assets/welcome.jpg';
+import React from "react";
+import styled from "@emotion/styled";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./components/theme/theme";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.colors.primary};
+`;
 
 const App: React.FunctionComponent = () => (
-  <img src={welcomeIcon} alt="Welcome!"/>
+  <ThemeProvider theme={theme}>
+    <Title>Hello</Title>
+  </ThemeProvider>
 );
 
 export default App;
